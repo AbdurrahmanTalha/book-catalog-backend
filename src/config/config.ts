45796be5aad3de4b95954c279e6fecc00b6a8913
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-07-16 22:46:28 +0600

    change

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import "dotenv/config";
 
 export default {
     port: process.env.PORT,
